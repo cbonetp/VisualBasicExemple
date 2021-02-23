ee6d87809f2cec294ec595aefdd76dababd2c29f
carles.bonet@escolapia.cat <carles.bonet@mataro.epiaedu.cat>
2021-02-23 11:56:31 +0100

    Visual Basic Segon Commit

--- EjemploGit/EjemploGit/Form1.vb
+++ EjemploGit/EjemploGit/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("hola")
-
+        Dim x As Integer
+        For x = 0 To 100
+            MsgBox(x)
+        Next
     End Sub
 End Class
