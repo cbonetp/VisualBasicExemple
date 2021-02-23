e3b37dee12e8c70a185e0ff263de75cef9b09003
carles.bonet@escolapia.cat <carles.bonet@mataro.epiaedu.cat>
2021-02-23 12:15:35 +0100

    Modificacio 2n canvi

---
+++ EjemploGit/EjemploGit/Form2.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim x As Integer
+        For x = 0 To 100
+            MsgBox("Hola" + x)
+
+        Next
+    End Sub
+End Class
